e6ce8d9ebabbfe45be23bfb053f3b3e5beefbe79
pierrevaut <p.vautherin@hotmail.com>
2020-05-08 13:26:18 +0200

    [ADD] propTypes

--- src/components/pageblock/PageBlock.jsx
+++ src/components/pageblock/PageBlock.jsx
@@ -1,9 +1,14 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import './PageBlock.css';
+import PropTypes from 'prop-types';
 
 
 const PageBlock = ({ className }) => (
   <div className={className} data-testid={`page-block-${className}`}>toto</div>
 );
+
+PageBlock.propTypes = {
+  className: PropTypes.string.isRequired,
+};
 
 export default PageBlock;
