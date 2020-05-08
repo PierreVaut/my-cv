14787e5f4029778951e3423f90ae7710087dc285
pierrevaut <p.vautherin@hotmail.com>
2020-05-08 11:27:06 +0200

    [ADD]  Page Component

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 import Page from './page';
+import Item from './item';
 
-export { Page };
+export { Page, Item };
--- src/components/page/Page.jsx
+++ src/components/page/Page.jsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import Item from '../item';
 import './Page.css';
+import ResumeContext from '../../context';
 
-const Page = () => (
-  <div className="A4" data-testid="page">
-  </div>
-);
+
+const Page = () => {
+  const resume = useContext(ResumeContext);
+  console.log(resume);
+  return (
+    <div className="A4" data-testid="page">
+      <div className="page-block left">
+        <Item type="" />
+        <Item type="" />
+        <Item type="" />
+
+      </div>
+      <div className="page-block right">
+        <Item type="" />
+        <Item type="" />
+        <Item type="" />
+
+      </div>
+    </div>
+  );
+};
 
 export default Page;
