9e474059af878d2c666bda2083bf74ac7ce8bd17
pierrevaut <p.vautherin@hotmail.com>
2020-05-09 17:01:55 +0200

    [ADD] content: skills, summary

--- src/context/resources/skills.js
+++ src/context/resources/skills.js
@@ -1,8 +1,99 @@
 export default {
-  skills: [
+  languages: [
     {
       order: 0,
-      title: 'Javscript',
+      title: 'Javascript ES6+ (confirmed)',
+      show: true,
+    },
+    {
+      order: 1,
+      title: 'Python (beginner)',
+      show: true,
+    },
+    {
+      order: 1,
+      title: 'HTML CSS',
+      show: true,
+    },
+  ],
+  'Libs & frameworks': [
+    {
+      order: 0,
+      title: 'React',
+      show: true,
+    },
+    {
+      order: 1,
+      title: 'Redux - Saga',
+      show: true,
+    },
+    {
+      order: 2,
+      title: 'Reselect - Recompose',
+      show: true,
+    },
+    {
+      order: 2,
+      title: 'Immutability helper',
+      show: true,
+    },
+  ],
+  backend: [
+    {
+      order: 0,
+      title: 'Node.js',
+      show: true,
+    },
+    {
+      order: 1,
+      title: 'REST, GraphQL',
+      show: true,
+    },
+    {
+      order: 2,
+      title: 'PostgreSQL, MariaDB',
+      show: true,
+    },
+    {
+      order: 2,
+      title: 'Mongo, DynamoDB',
+      show: true,
+    },
+  ],
+  devops: [
+    {
+      order: 0,
+      title: 'AWS Lambda, S3, EC2',
+      show: true,
+    },
+    {
+      order: 1,
+      title: 'Ansible',
+      show: true,
+    },
+  ],
+  versionning: [
+    {
+      order: 0,
+      title: 'Git',
+      show: true,
+    },
+
+  ],
+  project: [
+    {
+      order: 0,
+      title: 'Agile (Kanban & Scrum)',
+      show: true,
+    },
+    {
+      order: 1,
+      title: 'JIRA, Confluence',
+      show: true,
+    },
+    {
+      order: 1,
+      title: 'Github, Gitlab',
       show: true,
     },
   ],
