a6f1ac57495e0dadad228d9234d07fa6f84c9e26
pierrevaut <p.vautherin@hotmail.com>
2020-05-10 11:24:33 +0200

    [UPDATE] Content

--- src/context/resources/resume.js
+++ src/context/resources/resume.js
@@ -1,80 +1,95 @@
-// itemBlock level resources
+import educationLogo from '../../assets/educ.png';
+import workLogo from '../../assets/work.png';
+
+
 export default {
-  education: [
-    {
-      order: 0,
-      timePeriod: '2008',
-      title: 'Master’s degree in Communication',
-      place: 'University Paris Sorbonne',
-      show: true,
-    },
-    {
-      order: 1,
-      timePeriod: '2016-17',
-      title: 'Full stack Javascript Training',
-      place: 'IFOCOP',
-      show: true,
-    },
-    {
-      order: 2,
-      timePeriod: '2017-18',
-      title: 'Training in Javascript and Python',
-      place: 'Self-teaching',
-      show: true,
-    },
-  ],
-  experience: [
-    {
-      order: 0,
-      timePeriod: '2008-2011',
-      title: 'Project manager',
-      place: 'Cairn.info',
-      show: true,
-    },
-    {
-      order: 1,
-      timePeriod: '2012-2017',
-      title: 'Project manager',
-      place: 'Elsevier',
-      show: true,
-    },
-    {
-      order: 2,
-      timePeriod: 'Feb 2018-July 2018',
-      title: 'Fullstack developer Javascript',
-      place: 'Groupeer Technologies',
-      show: true,
-    },
-    {
-      order: 3,
-      timePeriod: 'July 2018-October 2018',
-      title: 'Backend developer Python',
-      place: 'SFR',
-      show: true,
-    },
-    {
-      order: 4,
-      timePeriod: 'November 2018-present',
-      title: 'Software Engineer',
-      place: 'Photobox',
-      show: true,
-    },
-  ],
-  miscellaneous: [
-    {
-      order: 0,
-      title: 'Native speaker French, fluent in English.',
-      show: true,
-    },
-    {
-      order: 1,
-      title: 'Passionate traveller. Fond of running, trail and mountain bike.',
-      show: true,
-    },
-    {
-      order: 2,
-      title: 'I really love cinema, especially Kubrick, Visconti and Tarkovsky.',
-      show: true,
-    },
-  ],
+  experience: {
+    visible: true,
+    logo: workLogo,
+    items: [
+      {
+        order: 0,
+        timePeriod: '2008-2011',
+        title: 'Project manager',
+        place: 'Cairn.info',
+        show: true,
+      },
+      {
+        order: 1,
+        timePeriod: '2012-2017',
+        title: 'Project manager',
+        place: 'Elsevier',
+        show: true,
+      },
+      {
+        order: 2,
+        timePeriod: 'Feb 2018-July 2018',
+        title: 'Fullstack developer Javascript',
+        place: 'Groupeer Technologies',
+        show: true,
+      },
+      {
+        order: 3,
+        timePeriod: 'July 2018-October 2018',
+        title: 'Backend developer Python',
+        place: 'SFR',
+        show: true,
+      },
+      {
+        order: 4,
+        timePeriod: 'November 2018-present',
+        title: 'Software Engineer',
+        place: 'Photobox',
+        show: true,
+      },
+    ],
+  },
+  education: {
+    visible: true,
+    logo: educationLogo,
+    items: [
+      {
+        order: 0,
+        timePeriod: '2008',
+        title: 'Master’s degree in Communication',
+        place: 'University Paris Sorbonne',
+        show: true,
+      },
+      {
+        order: 1,
+        timePeriod: '2016-17',
+        title: 'Full stack Javascript Training',
+        place: 'IFOCOP',
+        show: true,
+      },
+      {
+        order: 2,
+        timePeriod: '2017-18',
+        title: 'Training in Javascript and Python',
+        place: 'Self-teaching',
+        show: true,
+      },
+    ],
+  },
+  miscellaneous: {
+    visible: true,
+    logo: educationLogo,
+    items: [
+      {
+        order: 0,
+        title: 'Native speaker French, fluent in English.',
+        show: true,
+      },
+      {
+        order: 1,
+        title: 'Passionate traveller. Fond of running, trail and mountain bike.',
+        show: true,
+      },
+      {
+        order: 2,
+        title: 'I really love cinema, especially Kubrick, Visconti and Tarkovsky.',
+        show: true,
+      },
+    ],
+  },
 };
--- src/context/resources/skills.js
+++ src/context/resources/skills.js
@@ -1,100 +1,124 @@
 export default {
-  languages: [
-    {
-      order: 0,
-      title: 'Javascript ES6+ (confirmed)',
-      show: true,
-    },
-    {
-      order: 1,
-      title: 'Python (beginner)',
-      show: true,
-    },
-    {
-      order: 2,
-      title: 'HTML CSS',
-      show: true,
-    },
-  ],
-  'Libs & frameworks': [
-    {
-      order: 0,
-      title: 'React',
-      show: true,
-    },
-    {
-      order: 1,
-      title: 'Redux - Saga',
-      show: true,
-    },
-    {
-      order: 2,
-      title: 'Reselect - Recompose',
-      show: true,
-    },
-    {
-      order: 3,
-      title: 'Immutability helper',
-      show: true,
-    },
-  ],
-  backend: [
-    {
-      order: 0,
-      title: 'Node.js',
-      show: true,
-    },
-    {
-      order: 1,
-      title: 'REST, GraphQL',
-      show: true,
-    },
-    {
-      order: 2,
-      title: 'PostgreSQL, MariaDB',
-      show: true,
-    },
-    {
-      order: 3,
-      title: 'Mongo, DynamoDB',
-      show: true,
-    },
-  ],
-  devops: [
-    {
-      order: 0,
-      title: 'AWS Lambda, S3, EC2',
-      show: true,
-    },
-    {
-      order: 1,
-      title: 'Ansible',
-      show: true,
-    },
-  ],
-  versionning: [
-    {
-      order: 0,
-      title: 'Git',
-      show: true,
-    },
+  languages: {
+    visible: true,
+    logo: null,
+    items: [
+      {
+        order: 0,
+        title: 'Javascript ES6+ (confirmed)',
+        show: true,
+      },
+      {
+        order: 1,
+        title: 'Python (beginner)',
+        show: true,
+      },
+      {
+        order: 2,
+        title: 'HTML CSS',
+        show: true,
+      },
+    ],
+  },
+  'Libs & frameworks': {
+    visible: true,
+    logo: null,
+    items: [
+      {
+        order: 0,
+        title: 'React',
+        show: true,
+      },
+      {
+        order: 1,
+        title: 'Redux - Saga',
+        show: true,
+      },
+      {
+        order: 2,
+        title: 'Reselect - Recompose',
+        show: true,
+      },
+      {
+        order: 3,
+        title: 'Immutability helper',
+        show: true,
+      },
+    ],
+  },
+  backend: {
+    visible: true,
+    logo: null,
+    items: [
+      {
+        order: 0,
+        title: 'Node.js',
+        show: true,
+      },
+      {
+        order: 1,
+        title: 'REST, GraphQL',
+        show: true,
+      },
+      {
+        order: 2,
+        title: 'PostgreSQL, MariaDB',
+        show: true,
+      },
+      {
+        order: 3,
+        title: 'Mongo, DynamoDB',
+        show: true,
+      },
+    ],
+  },
+  devops: {
+    visible: true,
+    logo: null,
+    items: [
+      {
+        order: 0,
+        title: 'AWS Lambda, S3, EC2',
+        show: true,
+      },
+      {
+        order: 1,
+        title: 'Ansible',
+        show: true,
+      },
+    ],
+  },
+  versionning: {
+    visible: true,
+    logo: null,
+    items: [
+      {
+        order: 0,
+        title: 'Git',
+        show: true,
+      },
 
-  ],
-  project: [
-    {
-      order: 0,
-      title: 'Agile (Kanban & Scrum)',
-      show: true,
-    },
-    {
-      order: 1,
-      title: 'JIRA, Confluence',
-      show: true,
-    },
-    {
-      order: 2,
-      title: 'Github, Gitlab',
-      show: true,
-    },
-  ],
+    ],
+  },
+  project: {
+    visible: true,
+    logo: null,
+    items: [
+      {
+        order: 0,
+        title: 'Agile (Kanban & Scrum)',
+        show: true,
+      },
+      {
+        order: 1,
+        title: 'JIRA, Confluence',
+        show: true,
+      },
+      {
+        order: 2,
+        title: 'Github, Gitlab, Jenkins',
+        show: true,
+      },
+    ],
+  },
 };
