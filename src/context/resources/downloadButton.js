c8753488ffec945709b7d36d485eb51e4db44b6b
pierrevaut <p.vautherin@hotmail.com>
2020-05-16 13:59:52 +0200

    [UPDATE] link to S3

--- src/context/resources/downloadButton.js
+++ src/context/resources/downloadButton.js
@@ -3,5 +3,5 @@ import icon from '../../assets/download.png';
 export default {
   text: 'download PDF',
   icon,
-  link: '/CV-web-PVautherin_EN_2020.pdf',
+  link: 'https://cv-pierrevautherin.s3.amazonaws.com/CV_Pvautherin_EN_2020.pdf',
 };
